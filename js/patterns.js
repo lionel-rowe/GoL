24a2db6c042a300a4d585db4c660f3cf4bea33f0
Lionel Rowe <lionel.rowe@gmail.com>
2017-11-16 02:40:54 +0800

    Move all patterns to a single module

---
+++ js/patterns.js
@@ -0,0 +1,59 @@
+export const patterns = {
+  copperhead: `#N Copperhead
+#O 'zdr'
+#C An c/10 orthogonal spaceship found on March 5, 2016.
+#C http://www.conwaylife.com/wiki/Copperhead
+x = 8, y = 12, rule = B3/S23
+b2o2b2o$3b2o$3b2o$obo2bobo$o6bo2$o6bo$b2o2b2o$2b4o2$3b2o$3b2o!
+`,
+  fcc: `#N freeCodeCamp
+#C https://www.freecodecamp.org/
+x = 25, y = 16, rule = B3/S23
+3b2o15b2o$2b2o7b2o8b2o$b2o9b2o8b2o$b2o9b2o8b2o$2o9b3obo7b2o$2o9b3ob2o6
+b2o$2o8b2ob4o6b2o$2o7b3o2b4o5b2o$2o7b3o2bob2o5b2o$2o7b2o5b2o5b2o$2o7b2
+o5bo6b2o$2o8bo4bo7b2o$b2o19b2o$b2o4b11o4b2o$2b2o3b11o3b2o$3b2o15b2o!
+`,
+  gosper_gun: `#N Gosper glider gun_synth
+#C Glider synthesis of Gosper glider gun.
+#C www.conwaylife.com/wiki/index.php?title=Gosper_glider_gun
+x = 47, y = 14, rule = b3/s23
+16bo30b$16bobo16bo11b$16b2o17bobo9b$obo10bo21b2o10b$b2o11b2o31b$bo11b
+2o32b3$10b2o20b2o13b$11b2o19bobo9b3o$10bo21bo11bo2b$27bo17bob$27b2o18b
+$26bobo!`,
+  lightweight_spaceship: `#N Lightweight spaceship
+#O John Conway
+#C A very well-known period 4 c/2 orthogonal spaceship.
+#C www.conwaylife.com/wiki/index.php?title=Lightweight_spaceship
+x = 5, y = 4, rule = B3/S23
+bo2bo$o4b$o3bo$4o!
+`,
+  pentadecathlon: `#N Pentadecathlon
+#O John Conway
+#C 10 cells placed in a row evolve into this object, which is the most natural oscillator of period greater than 3. In fact, it is the fifth or sixth most common oscillator overall, being about as frequent as the clock, but much less frequent than the blinker, toad, beacon or pulsar.
+#C www.conwaylife.com/wiki/index.php?title=Pentadecathlon
+x = 10, y = 1, rule = B3/S23
+10o!
+`,
+  pulsar: `#N Pulsar
+#O John Conway
+#C A period 3 oscillator. Despite its size, this is the fourth most common oscillator (and by
+#C far the most common of period greater than 2).
+#C www.conwaylife.com/wiki/index.php?title=Pulsar
+x = 13, y = 13, rule = B3/S23
+2b3o3b3o2b2$o4bobo4bo$o4bobo4bo$o4bobo4bo$2b3o3b3o2b2$2b3o3b3o2b$o4bob
+o4bo$o4bobo4bo$o4bobo4bo2$2b3o3b3o!`,
+  tumbler: `#N Tumbler
+#O George Collins
+#C The smallest known period 14 oscillator.
+#C www.conwaylife.com/wiki/index.php?title=Tumbler
+x = 9, y = 5, rule = B3/S23
+bo5bob$obo3bobo$o2bobo2bo$2bo3bo2b$2b2ob2o!`,
+  twin_bees: `#N Twin bees shuttle v2
+#O David Bell, Bill Gosper and Heinrich Koenig
+#C Some alternate stabilizations of the period 46 twin bees shuttle os
+#C cillator.
+#C www.conwaylife.com/wiki/index.php?title=Twin_bees_shuttle
+x = 27, y = 17, rule = b3/s23
+b2o24b$b2o24b2$15b2o10b$2o13bobo9b$2o15bo9b$15b3o5bo3b$23b3ob$26bo$23b
+3ob$15b3o5bo3b$2o15bo9b$2o13bobo9b$15b2o10b2$b2o24b$b2o!`
+};
