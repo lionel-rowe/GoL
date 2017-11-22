3eaef0ecb48ecba55753b30b402e7a5cdda5b45c
Lionel Rowe <lionel.rowe@gmail.com>
2017-11-22 22:27:10 +0800

    Optimize neighbor checking algo

--- js/math.js
+++ js/math.js
@@ -1,11 +1,30 @@
 import {state} from './state.js';
 
+import {offScreenSpaceBefore, gameWidth, gameHeight} from './config.js';
+
+const neighborArray = new Array(offScreenSpaceBefore * 2 + gameHeight).fill(0).map(el => new Array(offScreenSpaceBefore * 2 + gameWidth)); // more performant than recalculating neighbor coords every time
+
+for (let rowIdx = 0; rowIdx < neighborArray.length; rowIdx++) {
+  for (let cellIdx = 0; cellIdx < neighborArray[rowIdx].length; cellIdx++) {
+    neighborArray[rowIdx][cellIdx] = [
+      [rowIdx - 1, cellIdx - 1],
+      [rowIdx - 1, cellIdx    ],
+      [rowIdx - 1, cellIdx + 1],
+      [rowIdx,     cellIdx - 1],
+      [rowIdx,     cellIdx + 1],
+      [rowIdx + 1, cellIdx - 1],
+      [rowIdx + 1, cellIdx    ],
+      [rowIdx + 1, cellIdx + 1]
+    ];
+  }
+}
+
 export function getNeighborScore(cellRef) {
   let neighborScore = 0;
-  const neighbors = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+  const neighbors = neighborArray[cellRef[0]][cellRef[1]];
 
   neighbors.forEach((neighbor) => {
-    neighborScore += state.gameArray[cellRef[0] + neighbor[0]][cellRef[1] + neighbor[1]];
+    neighborScore += state.gameArray[neighbor[0]][neighbor[1]];
   });
 
   return neighborScore;
